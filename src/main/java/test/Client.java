2684c10b96fa5cf0f5458285590f8c711ad450c4
sancar <sancar@hazelcast.com>
2014-12-02 11:25:27 +0200

    issue #4186

--- src/main/java/test/Client.java
+++ src/main/java/test/Client.java
@@ -1,8 +1,12 @@
 package test;
 
+import com.hazelcast.core.EntryAdapter;
+import com.hazelcast.core.EntryEvent;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
 
 import java.io.IOException;
+import java.util.Random;
 
 /**
  * User: sancar
@@ -13,6 +17,34 @@ public class Client {
 
     public static void main(String[] args) throws IOException {
         final HazelcastInstance client = Util.createClient();
+        final IMap<Object, Boolean> mapC = client.getMap("default");
+
+        mapC.addEntryListener(new EntryAdapter<Object, Boolean>() {
+            @Override
+            public void onEntryEvent(EntryEvent<Object, Boolean> event) {
+                System.err.println("mapC event = " + event.getKey());
+            }
+        }, true);
+
+
+        final Random random = new Random();
+
+        final Thread clientThread = new Thread() {
+            @Override
+            public void run() {
+
+                while (true) {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    mapC.put(random.nextInt(), true);
+                }
+            }
+        };
+        clientThread.start();
+
 
     }
 }
--- src/main/java/test/Node.java
+++ src/main/java/test/Node.java
@@ -1,6 +1,9 @@
 package test;
 
+import com.hazelcast.core.EntryAdapter;
+import com.hazelcast.core.EntryEvent;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
 
 /**
  * User: sancar
@@ -12,6 +15,16 @@ public class Node {
 
     public static void main(String[] args) throws Exception {
         final HazelcastInstance server = Util.createServer();
+        final IMap<Object, Boolean> map = server.getMap("default");
+
+        map.addEntryListener(new EntryAdapter<Object, Boolean>() {
+            @Override
+            public void onEntryEvent(EntryEvent<Object, Boolean> event) {
+                System.err.println("map event = " + event.getKey());
+            }
+        }, true);
+
+
     }
 
 }
