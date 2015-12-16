72a30eda3815a7b9cd188717bee562bf89cd9894
sancar <sancar@hazelcast.com>
2015-12-16 14:44:16 -0800

    wip

--- runClient.sh
+++ runClient.sh
@@ -6,9 +6,9 @@ HOSTNAME=`echo $HOSTNAME | awk '{print tolower($0)}'`
 
 MAVEN_OPTS="-Xmx1G"
 
-for (( i=0; i<1; i++ ))
+for (( i=0; i<200; i++ ))
 do
    #                                                         keyRange threadCount clientId serverId testDurationSeconds warmupDurationSeconds
-   mvn exec:java -Dexec.mainClass="test.Client" -Dexec.args="5000          1         $i    $HOSTNAME         10                   5" &
+   mvn exec:java -Dexec.mainClass="test.Client" -Dexec.args="1000000          1         $i    $HOSTNAME         120                   30" &
 done
 
--- runNode.sh
+++ runNode.sh
@@ -4,7 +4,7 @@ HOSTNAME=`hostname`
 
 HOSTNAME=`echo $HOSTNAME | awk '{print tolower($0)}'`
 
-MAVEN_OPTS="-server -Xmx5G"
+MAVEN_OPTS="-server -Xmx8G"
 
 #                                                       numberOfItems valueSizeKB $HOSTNAME
-mvn exec:java -Dexec.mainClass="test.Node" -Dexec.args=" 1000             1      $HOSTNAME"
+mvn exec:java -Dexec.mainClass="test.Node" -Dexec.args=" 15000000           1000  $HOSTNAME"
